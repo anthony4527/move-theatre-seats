0838b2e367b002c1ce6100d5876d94abaf91d326
AnthonyLee <anthony@calglobal.com>
2023-02-25 22:11:22 +0800

    refactor to use hashmap for row label A,B, ...

--- src/main/java/com/techreturn/Cinema.java
+++ src/main/java/com/techreturn/Cinema.java
@@ -1,15 +1,27 @@
 package com.techreturn;
 
+import java.util.Map;
+import java.util.HashMap;
+
+
 public class Cinema {
-public final int MaxRow = 3;
+    public final int MaxRow = 3;
     public final int MaxRowSeat = 5;
 
     MovieSeat[][] listOfSeats = new MovieSeat[MaxRow][MaxRowSeat];
+    // a map of the array index to label
+    Map<Integer, String> labelMap = new HashMap<Integer, String>();
+
     //init all seat as available
     public Cinema(){
+        String label;
+        labelMap.put(0, "A");
+        labelMap.put(1, "B");
+        labelMap.put(2, "C");
         // set each
-        String label = " ";
         for (int i=0; i< MaxRow; i++){
+            label = labelMap.get(i);
+            /*
             switch (i) {
                 case 0:
                     label = "A";
@@ -20,7 +32,7 @@ public final int MaxRow = 3;
                 case 2:
                     label = "C";
                     break;
-            }
+            }*/
             for (int j=0; j<MaxRowSeat; j++){
                 this.listOfSeats[i][j] = new MovieSeat(label,j+1);
             }
